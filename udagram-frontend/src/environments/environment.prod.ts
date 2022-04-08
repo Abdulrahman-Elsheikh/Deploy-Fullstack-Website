2fb864f20d89f18cd05f5f6510e8217a58e4220c
Abdulrahman Elsheikh <eng.abdulrahman.elsheikh@gmail.com>
2022-04-08 13:33:40 +0200

    URL With Version

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'Udagramapi-env.eba-kpiynjrk.us-east-1.elasticbeanstalk.com',
+  apiHost: 'Udagramapi-env.eba-kpiynjrk.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
